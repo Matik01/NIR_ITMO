fb3cf18da69e83b7603ffeba0fde2c627b6dbc82
Matik01 <piupip4@gmail.com>
2024-12-08 18:05:19 +0300

    added pml.pdb deleting by rmsd

--- util/calculate_docking_rmsd.py
+++ util/calculate_docking_rmsd.py
@@ -38,10 +38,16 @@ ref_smarts = Chem.MolToSmarts(ref_mol)
 
 for dir in directories:
     exp_id = dir.split(sep='/')[2].split(sep="@")[0]
-    id_best_rmsd[exp_id] = [1, 9999] #pose/rms values
+    exp_directory = dir.split(sep='/')[2]
+    id_best_rmsd[exp_id] = [0, 9999] #pose/rms values
     pdb_files = []
+    main_dir = None
+
     for dir_exp, subdir, files in os.walk(dir):
+        if main_dir is None:
+            main_dir = dir_exp
         pdb_files.extend([file for file in files if "pml.pdb" in file])
+    
     for conf_pdb in pdb_files:
         mols = []
         mol = Chem.MolFromPDBFile(f'{dir}/{conf_pdb}', removeHs=True)
@@ -54,8 +60,14 @@ for dir in directories:
         molMatch = mol.GetSubstructMatch(pattern)
         rms = Chem.rdMolAlign.CalcRMS(mol, ref_mol, map=[list(zip(molMatch, refMatch))])
         if rms < id_best_rmsd.get(exp_id)[1]:
+            if id_best_rmsd.get(exp_id)[1] == 9999:
+                pass
+            else:
+                os.remove(f'{main_dir}/{exp_directory}.{id_best_rmsd.get(exp_id)[0]}.pml.pdb')
             id_best_rmsd[exp_id] = [conf_pdb.split(sep='.')[1], rms]
-        
+        elif rms > id_best_rmsd.get(exp_id)[1]: 
+            os.remove(main_dir + "/" + conf_pdb)
+
 df = pd.DataFrame(
     [(key, value[0], value[1]) for key, value in id_best_rmsd.items()], 
     columns=['id', 'pose', 'rmsd_value']
