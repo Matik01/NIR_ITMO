b96252127f880465b1dc17365af48769924ba948
Matik01 <piupip4@gmail.com>
2025-01-10 22:11:27 +0300

    Added basic BPS function

--- util/calculate_descriptors.py
+++ util/calculate_descriptors.py
@@ -3,6 +3,7 @@ from entity.protein import Protein
 from entity.constants import Constants
 from sklearn.neighbors import KDTree
 import numpy as np
+from itertools import product
 
 
 def read_pdb(filename: str) -> (Ligand, Protein):
@@ -23,8 +24,8 @@ def process_ligand(ligand_data: list) -> Ligand:
     Getting atom types and coords for each
     '''
     for item in ligand_data:
-        atom_type = item[76:78]
-        atom_types.append(atom_type.strip())
+        atom_char = item[76:78]
+        atom_types.append(atom_char.strip())
         x = item[32:38]
         y = item[40:46]
         z = item[48:54]
@@ -64,39 +65,73 @@ def process_protein(protein_data: list) -> Protein:
 
 
 def fc_cutoff(r_distance: float) -> float:
-    if r_distance <= Constants.RC_CUTOFF_DISTANCE:
-        return np.tanh(1 - r_distance / Constants.RC_CUTOFF_DISTANCE) ** 3
-    else:
-        return 0
+    return np.tanh(1 - r_distance / Constants.RC_CUTOFF_DISTANCE) ** 3
 
 
-def distance_function(eta: float, r_distance: float, r_s: float) -> float:
-    return np.sum(np.exp(-eta * (r_distance - r_s) ** 2) * fc_cutoff(r_distance))  # i != j
+def distance_function(eta: float, distance_ij: float, r_s: float) -> float:
+    return np.sum(np.exp(-eta * (distance_ij - r_s) ** 2) * fc_cutoff(distance_ij))  # i != j
 
 
-def angle_function(zeta: float, eta: float, cos_teta_ijk: float,
+def angle_function(zeta: float, eta: float, cosine_teta: float,
                    distance_ij: float, distance_jk: float, distance_ik: float) -> list:
     angle_descriptors = []
     for lambda_value in Constants.LAMBDA_VALUES:
-        g_angular = 2 ** (1 - zeta) * np.sum((1 + lambda_value * cos_teta_ijk) ** zeta * np.exp(
+        g_angular = 2 ** (1 - zeta) * np.sum((1 + lambda_value * cosine_teta) ** zeta * np.exp(
             -eta * (distance_ij ** 2 + distance_jk ** 2 + distance_ik ** 2)) * fc_cutoff(distance_ij) * fc_cutoff(
             distance_jk) * fc_cutoff(distance_ik))
         angle_descriptors.append(g_angular)
 
     return angle_descriptors
 
+def bps_compute(prot: Protein, ligand: Ligand) -> list:
+    env_descriptors = list()
+
+    for atom_char in prot.get_coords().keys():
+        idx, distances = prot.get_kd_trees()[atom_char].query_radius(ligand.get_coords(),
+                                                                     Constants.RC_CUTOFF_DISTANCE,
+                                                                     return_distance=True)
+        atom_type_list = list()
+        for atom_id in range(ligand.get_coords().shape[0]):
+            descriptors = list()
+            env_coords = prot.get_coords()[atom_char][idx[atom_id], :]
+            ligand_atom_coords = ligand.get_coords()[atom_id]
+
+            j = np.tile(env_coords, (env_coords.shape[0], 1))
+            k = np.repeat(env_coords, env_coords.shape[0], axis=0)
+            distance_ij = np.sum((ligand_atom_coords - j) ** 2, axis=1) ** 0.5
+            distance_ik = np.sum((ligand_atom_coords - k) ** 2, axis=1) ** 0.5
+            distance_jk = np.sum((j - k) ** 2, axis=1) ** 0.5
+            cosine_teta = np.sum((ligand_atom_coords - j) * (ligand_atom_coords - k), axis=1) / (
+                    distance_ij * distance_ik)
+
+            for result in cosine_teta:
+                if int(result) > 1:
+                    print(f'Error not valid cosine: {result}')
+
+            for eta, rs in product(Constants.ETA_VALUES, Constants.RS_VALUES):
+                temp = distance_function(eta=eta, distance_ij=distance_ij, r_s=rs)
+                descriptors.append(temp)
+
+            for l in Constants.LAMBDA_VALUES:
+                for eta, zeta in product(Constants.ETA_VALUES, Constants.ZETA_VALUES):
+                    temp = angle_function(zeta=zeta, eta=eta, cosine_teta=cosine_teta, distance_ij=distance_ij,
+                                          distance_ik=distance_ik, distance_jk=distance_jk)
+                    descriptors.append(temp)
+            atom_type_list.append(descriptors)
+
+        env_descriptors.append(atom_type_list)
+    return env_descriptors
 
 if __name__ == '__main__':
     files = ['test_system.pdb']  # TO ADD: Directory reader
     ligand_list: list[Ligand] = []
     protein_list: list[Protein] = []
 
-    for file in files:  # Do i really need this?
+    for file in files:  # Do I really need this?
         mol, prot = read_pdb(file)
         ligand_list.append(mol)
         protein_list.append(prot)
 
-    out = list()
+
     for prot, ligand in zip(protein_list, ligand_list):
-        for atom_type in prot.get_coords().keys():
-            print(prot.get_kd_trees()[atom_type].query_radius(ligand.get_coords(), Constants.RC_CUTOFF_DISTANCE, return_distance=True))
+        bps_compute(prot=prot, ligand=ligand)
